268040bf90fe029086e6fbd499a475f4473c2c82
Sabelius <sbebby92@virgilio.it>
2021-09-14 14:58:55 +0200

    completamento esercizio

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+let root = new Vue ({
+    el : "#root",
+
+    data : {
+        message : "The bugs in the code.",
+        image : "https://www.memesmonkey.com/images/memesmonkey/90/90a94e2a3da3bffbdd3c2a49625c82e6.jpeg",
+    }
+
+});
